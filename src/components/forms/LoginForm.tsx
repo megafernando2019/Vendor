8524b30b95e39fadbaf2272873d7083c98a90c33
Israel Duran <aduran@megatravel.com.mx>
2026-07-13 12:46:35 -0600

    Se quita prueba de mensaje en login

--- src/components/forms/LoginForm.tsx
+++ src/components/forms/LoginForm.tsx
@@ -56,7 +56,7 @@ export default function LoginForm({ redirectTo }: LoginFormProps) {  const route
       const message =
         typeof data?.message === "string" && data.message.trim().length > 0
           ? data.message
-          : `Error ${res.status} ${res.message}`;
+          : `Error ${res.status}`;
 
       if (res.ok && data?.success) {
         toast.success("Bienvenido");
